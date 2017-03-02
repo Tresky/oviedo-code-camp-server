0a396e83f92cf55f00c7a8dab183a7f61fe6ac2c
Matt Webb <mattrw89@gmail.com>
2017-03-01 19:18:02 -0500

    Add sabayon support (Lets Encrypt support)
    Got it from here: https://gist.githubusercontent.com/markcerqueira/2fe0d1ab7654febaa6735316b72b3882/raw/5b1c6181a765383161658583288a9b580280cb28/controller.rb

--- application.rb
+++ application.rb
@@ -11,6 +11,35 @@ end
 set :protection, false
 set :public_dir, Proc.new { File.join(root, "_site") }
 
+get '/.well-known/acme-challenge/:token' do
+    data = []
+    if ENV['ACME_KEY'] && ENV['ACME_TOKEN']
+      data << { key: ENV['ACME_KEY'], token: ENV['ACME_TOKEN'] }
+    else
+      ENV.each do |k, v|
+        if d = k.match(/^ACME_KEY_([0-9]+)/)
+          index = d[1]
+          data << { key: v, token: ENV["ACME_TOKEN_#{index}"] }
+        end
+      end
+    end
+
+    data.each do |e|
+      if env['PATH_INFO'] == "/acme-challenge/#{e[:token]}"
+        status 200
+        content_type 'text/plain'
+        body "#{e[:key]}"
+        "#{e[:key]}"
+        return
+      end
+    end
+
+    status 500
+    content_type 'text/plain'
+    body 'No key found'
+    'No key found'
+  end
+
 post '/send_email' do
   puts 'Endpoint Hit'
   puts 'Verifying ReCaptcha'
