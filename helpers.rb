333228b26e8f6915c47ca7d2588920322690d2c7
Tyler Petresky <tnpetresky@gmail.com>
2017-03-15 15:46:46 -0400

    Handle fields that are missing

--- helpers.rb
+++ helpers.rb
@@ -2,7 +2,7 @@ module Helpers
   def Helpers.pull_params(params, pull)
     res = {}
     pull.each { |keep|
-      if params.has_key?(keep)
+      if params.has_key?(keep) && !params[keep].eql?('na')
         res[keep.to_sym] = params[keep.to_s]
       else
         return false
